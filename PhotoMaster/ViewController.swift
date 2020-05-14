391890cb9102f77340e3f2547aa49cf44b4b9674
Hosho Kusunose <hosho.kusu@icloud.com>
2020-05-15 01:26:09 +0900

    once finished

--- PhotoMaster/ViewController.swift
+++ PhotoMaster/ViewController.swift
@@ -8,13 +8,120 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//UINavigationControllerDelegate, UIImagePickerControllerDelegateはカメラとアルバムから写真を取得する時に使う
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet var photoImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func onTappedCameraButton() {
+        presentPickController(sourceType: .camera)
+    }
+    
+    @IBAction func onTappedAlbumButton() {
+        presentPickController(sourceType: .photoLibrary)
+    }
+    
+    //「テキスト合成」押した時のメソッド
+    @IBAction func onTappedTextButton() {
+        if photoImageView.image != nil {
+            photoImageView.image = drawText(image: photoImageView.image!)
+        } else {
+            print("画像がありません")
+        }
+    }
+    
+    //「イラス合成」押した時のメソッド
+    @IBAction func onTappedIllustButton() {
+        if photoImageView.image != nil {
+            photoImageView.image = drawMaskImage(image: photoImageView.image!)
+        } else {
+            print("画像がありません")
+        }
+    }
+    
+    @IBAction func onTappedUploadButton() {
+        if photoImageView.image != nil {
+            //共有するアイテムを設定
+            let activityVC = UIActivityViewController(activityItems: [photoImageView.image!, "#PhotoMaster"], applicationActivities: nil)
+            self.present(activityVC, animated: true, completion: nil)
+        } else {
+            print("画像がありません")
+        }
+    }
+    
+    func presentPickController(sourceType: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(sourceType){
+            let picker = UIImagePickerController()
+            picker.sourceType = sourceType
+            picker.delegate = self
+            self.present(picker, animated: true, completion: nil)
+        }
+    }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    
+    self.dismiss(animated: true, completion: nil)
+        
+        photoImageView.image = info[.originalImage]as? UIImage
+    }
+    
+    //元の画像にテキストを合成するメソッド
+    func drawText(image: UIImage) -> UIImage {
+        
+        //テキスト内容の設定
+        let text = "Life is Tech!"
+        
+        //texyFontAttributes: 文字の特性[フォントとサイズ、カラー、スタイル]の設定
+        let textFontAttributes = [
+            NSAttributedString.Key.font: UIFont(name: "Arial", size: 120)!,
+            NSAttributedString.Key.foregroundColor: UIColor.red
+        ]
+        
+        //グラフィックコンテキスト
+        UIGraphicsBeginImageContext(image.size)
+        
+        //読み込んだ写真を書き出す
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        
+        //定義　CGRect(x: [左からのx座標]px, y: [上からのy座標]px, width: [横の長さ]px, heght: [縦の長さ]px)
+        let margin: CGFloat = 5.0 //余白
+        let textRect = CGRect(x: margin, y: margin, width: image.size.width - margin, height: image.size.height - margin)
+        
+        //textRectで指定した範囲にtextFontAttributesに従ってtextを書き出す
+        text.draw(in: textRect, withAttributes: textFontAttributes)
+        
+        //グラフィックコンテキストの画像を取得
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        //グラフィックコンテキストの編集を終了
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+    
+    func drawMaskImage(image: UIImage) -> UIImage {
+        //マスク画像(保存場所: PhotoMaster > Assets.xcassets)の設定
+        let maskImage = UIImage(named: "furo_ducky")!
+        
+        UIGraphicsBeginImageContext(image.size)
+        
+        image.draw(in: CGRect(x: 30, y: 0, width: image.size.width, height: image.size.height))
+        
+        let margin: CGFloat = 50.0
+        let maskRect = CGRect(x: image.size.width - maskImage.size.width - margin, y: image.size.height - maskImage.size.height - margin, width: maskImage.size.width, height: maskImage.size.height)
+        
+        maskImage.draw(in: maskRect)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
 }
 
